1d1feb4f98340d352b788870f23b8f43866c7f24
gerardmtz <gerartmartinez@gmail.com>
2023-09-24 07:54:38 -0600

    Versión funcional del cliente con correcta visualización

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import requests
 
-st.title('Basic Data Visualization App')
-st.text('This is an example app for datavisualization')
+st.title('OxiCheck')
+st.text('Visualización de la oxigenación a través de los últimos cinco días')
 
-st.header("Bar Chart")
+st.header("Oxigenación últimos 5 días:")
 
 # Column representation
  
@@ -20,6 +20,53 @@ response = requests.get("http://localhost:3000/api");
 data = response.json();
 print(data)
 
-df = pd.DataFrame(data)
-df
-st.line_chart(data=df)
+df1 = pd.DataFrame(data)
+df1
+print("\n")
+print(df1)
+print("\n")
+
+# del df1[df1.columns[0]]
+
+print(df1)
+
+#st.line_chart(data=df)*/
+
+
+#Get list of values
+Means = [];
+Min = [];
+Max = [];
+n=0;
+
+for i in list(df1):
+     dat = df1[i].tolist();
+     Means.append(dat[2]);
+     Min.append(dat[1]);
+     Max.append(dat[0]);
+
+print(Means)
+print(Min)
+print(Max)
+
+
+# for i in list(df1):
+#     print(df1[i].tolist())
+
+
+# dictionary
+dict = { 'Days': list(range(1,6)),
+    'Promedio': Means,
+    'Max': Max,
+    'Min': Min}
+
+print(dict)
+  
+# create a dataframe object
+df2 = pd.DataFrame(dict)
+
+st.line_chart(
+     
+     data=df2, x="Days", y=["Max", "Min", "Promedio"]
+     
+     )
