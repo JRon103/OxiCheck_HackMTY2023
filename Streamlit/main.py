f5e9c00039ca767735dc397f1ea5302faa8b9a9b
gerardmtz <gerartmartinez@gmail.com>
2023-09-23 21:15:28 -0600

    Se conectó el servidor de NodeJS con con Streamlit v1.0

---
+++ Streamlit/main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+st.title('Basic Data Visualization App')
+st.text('This is an example app for datavisualization')
+
+st.header("Line Chart")
+
+# Column representation
+
+""" 
+data = {
+            "First":[23,12, 78, 4, 54],
+            "Second": [0, 13, 88, 1, 3],
+            "Third": [45, 2, 546, 67, 56]
+        }
+"""
+
+response = requests.get("http://localhost:3000/api");
+data = response.json();
+
+df = pd.DataFrame(data)
+df
+st.line_chart(data=df)
--- server/index.routes.js
+++ server/index.routes.js
@@ -10,7 +10,12 @@ router.post('/api', (req, res, next) => {
 
 router.get('/api', (req, res, next) => {
     console.log('OK');
-    res.send('You got it!')   
+    // res.send('You got it!')
+    res.send({
+        "First":[23,12, 78, 4, 54],
+        "Second": [0, 13, 88, 1, 3],
+        "Third": [45, 2, 546, 67, 56]
+    });
 });
 
 module.exports = router;
